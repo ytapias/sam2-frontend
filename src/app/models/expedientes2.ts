d9698ea82c19b7d2e07ef11d9a6109b10f57670e
ytapias <yesid_tapias@hotmail.com>
2024-09-25 21:14:33 -0500

    27
    27

---
+++ src/app/models/expedientes2.ts
@@ -0,0 +1,59 @@
+ 
+export class Expedientes2 {
+  constructor( 
+    public id:number,
+    public idempresa:number,
+    public expediente:string,
+    public idpais:number,
+    public pais:string,
+    public dominio:string,
+    public anno:number,
+    public clase:string,
+    public tipomarca:number,
+    public fechapb:Date,
+    public idtipoproceso:number,
+    public tipoproceso:string,
+    public certif:string,
+    public ctlcert:string,
+    public fechaconb:Date,
+    public resolucion:string,
+    public tiporesolucion:string,
+    public fecharesolucionb:Date,
+    public fechavencimientob:Date,
+    public gaceta:string,
+    public tipoprod:string,
+    public observaciones:string,
+    public propia:string,
+    public coddenomi:number,
+    public idmarca:number,
+    public codapoderado:number,
+    public idapoderado:number,
+    public descprod:string,
+    public nrointerno:string,
+    public estad:string,
+    public prioridad:string,
+    public fechaprioridad:Date,
+    public expedienteprioridad:string,
+    public pag:string,
+    public yourref:string,
+    public aviso:string,
+    public licenuso:string,
+    public fechapublicacion:Date,
+    public fechainac:Date,
+    public imagen:string,
+    public clases:string,
+    public um:Date,
+    public ts:string,
+    public vcniza:string,
+    public rvidicac:string,
+    public tiposol:string,
+    public vigenuso:Date,
+    public frrenova:Date,
+    public idestado:number    
+      ) 
+  { }
+}
+
+
+  
+  
---
+++ src/app/services/expedientes2.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+
+import { Expedientes2 } from '../models/expedientes2';
+import { Empresas } from '../models/empresas.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+const base_url =environment.base_url;
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Expedientes2Service {
+
+  constructor(private http: HttpClient) { }
+
+  get token():string{
+    return localStorage.getItem('token') || '';
+  }
+
+  get headers(){
+    return {
+      headers:
+      {
+        'x-token':this.token
+          }
+
+      };
+  }
+
+ // public totalUsuarios :number =0;
+ //public tiposdetalle:Empresas[]=[];
+
+
+  cargar(desde: number =0,cuantos: number =10, idmarca :Number = 0, nombre :string="")
+  {
+    const url = `${ base_url }/expedientesSQL2?desde=${desde}&elementos=${cuantos}&idmarca=${idmarca}&nombre=${nombre}`;
+ console.log(url);
+
+    return this.http.get(url,this.headers);
+  }
+
+
+  crear(item : Expedientes2)
+  {
+      console.log('creando');
+      console.log(item);
+      
+      return this.http.post(`${ base_url }/expedientesSQL2/`,item,this.headers);
+  }
+
+  modificar(item : Expedientes2)
+  {
+      console.log('modificar');
+      console.log(item);
+      
+      const {id } =item;
+
+    var respuesta =this.http.put(`${ base_url }/expedientesSQL2/${id}`,item,this.headers) 
+    console.log(respuesta);
+    return respuesta;
+  }
+
+  eliminar(item : Expedientes2)
+  {
+    const url = `${ base_url }/expedientesSQL2/${item.id}`;
+    console.log(url);
+    return this.http.delete(url,this.headers);
+  }
+
+}
+
